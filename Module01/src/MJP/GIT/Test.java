133acdbcdb1c3d999c10066a3d23da607dc89a78
MJP <2228402871@qq.com>
2022-05-03 11:15:47 +0800

    定义两个属性

--- Module01/src/MJP/GIT/Test.java
+++ Module01/src/MJP/GIT/Test.java
@@ -1,10 +1,11 @@
 package MJP.GIT;
+import sun.management.Agent;
 
 /**
- * @Auther: Mjp
- * @Date: 2022/5/3 - 05 - 03 - 10:59
- * @Description: MJP.GIT
- * @version: 1.0
+ *
  */
 public class Test {
+    private int age;
+    private double height;
+
 }
